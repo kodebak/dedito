11d7956b0dc9772754085b6acc02c72459593dfa
kodeabje <kodeabe@proton.me>
2025-02-23 22:48:37 +0000

    trying to fix animation because it's gone

--- script.js
+++ script.js
@@ -9,11 +9,8 @@ const touchIndicators = {};
 // Function to handle touch start
 function handleTouchStart(event) {
     event.preventDefault();
-    // Add the fade-out class after the DOM has been updated
-    setTimeout(() => {
-        instruction.classList.add('fade-out');
-        instruction.classList.remove('fade-in');
-    }, 0);
+    instruction.classList.add('fade-out');
+    instruction.classList.remove('fade-in');
     for (let i = 0; i < event.changedTouches.length; i++) {
         const touch = event.changedTouches[i];
         // Create a new indicator for each touch point
@@ -44,11 +41,8 @@ function handleTouchMove(event) {
 // Function to handle touch end
 function handleTouchEnd(event) {
     event.preventDefault();
-    // Add the fade-in class after the DOM has been updated
-    setTimeout(() => {
-        instruction.classList.add('fade-in');
-        instruction.classList.remove('fade-out');
-    }, 0);
+    instruction.classList.add('fade-in');
+    instruction.classList.remove('fade-out');
     for (let i = 0; i < event.changedTouches.length; i++) {
         const touch = event.changedTouches[i];
         // Remove the indicator when the touch ends
